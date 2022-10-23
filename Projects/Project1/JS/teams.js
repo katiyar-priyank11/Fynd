1c0b00abbbf74c64095c59e4a5de31279970f494
Priyank Katiyar <priyankkatiyar11@gmail.com>
2022-10-23 21:41:34 +0530

    .

--- Projects/Project1/JS/teams.js
+++ Projects/Project1/JS/teams.js
@@ -2,7 +2,7 @@ function myFunction1() {
     var x = document.getElementById("add-team");
     var y = document.getElementById("add-team-form");
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "grid";
       y.style.display = "none";
     } else {
       x.style.display = "none";
